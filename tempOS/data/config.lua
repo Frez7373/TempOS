e226949cf9e12da16e8c0ca152bbdf0d92548113
Frez7373 <danilla.mikhailov@gmail.com>
2026-09-12 05:30:20 +0300

    TempOS: add config

---
+++ tempOS/data/config.lua
@@ -0,0 +1,9 @@
+return {
+  version="1.0.0",
+  name="TempOS",
+  computerName="TempComputer",
+  theme="default",
+  lockEnabled=false,
+  password="",
+  updateURL="https://raw.githubusercontent.com/Frez7373/TempOS/main/"
+}
