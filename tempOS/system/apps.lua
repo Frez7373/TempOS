c5ba656d2ae59d9bd3729b32b7480f818e8d0057
Frez7373 <danilla.mikhailov@gmail.com>
2026-09-12 05:34:45 +0300

    TempOS: point editor to working app

--- tempOS/system/apps.lua
+++ tempOS/system/apps.lua
@@ -1,7 +1,7 @@
 local A={}
 A.dir="/tempOS/apps"
 A.items={
- {name="Files",file="files.lua",icon="F"},{name="Settings",file="settings.lua",icon="S"},{name="Terminal",file="terminal.lua",icon=">"},{name="Editor",file="editor.lua",icon="E"},{name="Calculator",file="calculator.lua",icon="#"},{name="Clock",file="clock.lua",icon="C"},{name="Devices",file="devices.lua",icon="D"},{name="Network",file="network.lua",icon="N"},{name="Task Manager",file="taskmgr.lua",icon="T"},{name="Security",file="security.lua",icon="!"},{name="Update Manager",file="update.lua",icon="U"},{name="Package Manager",file="package.lua",icon="P"}
+ {name="Files",file="files.lua",icon="F"},{name="Settings",file="settings.lua",icon="S"},{name="Terminal",file="terminal.lua",icon=">"},{name="Editor",file="textedit.lua",icon="E"},{name="Calculator",file="calculator.lua",icon="#"},{name="Clock",file="clock.lua",icon="C"},{name="Devices",file="devices.lua",icon="D"},{name="Network",file="network.lua",icon="N"},{name="Task Manager",file="taskmgr.lua",icon="T"},{name="Security",file="security.lua",icon="!"},{name="Update Manager",file="update.lua",icon="U"},{name="Package Manager",file="package.lua",icon="P"}
 }
 function A.list() return A.items end
 function A.path(item) return A.dir.."/"..item.file end
